348f85bacfa9c2a1e3d5cee934576020eb3ce6c5
Олег Космачев <ok@MacBook-Pro-Oleg-2.local>
2020-10-24 21:30:46 +0300

    добавка изменений

---
+++ js/script.js
@@ -0,0 +1,99 @@
+
+$(function () {
+   $(".humburger span").on('click', function () {
+      $("nav").toggle();
+   });
+});
+
+$(function () {
+   $(window).scroll(function () {
+      if ($(this).scrollTop() > 500) {
+         $('.scrollUp').fadeIn();
+      } else {
+         $('.scrollUp').fadeOut();
+      }
+   });
+
+   $('.scrollUp').click(function () {
+      $("html, body").animate({
+         scrollTop: 0
+      }, 700);
+      return false;
+   });
+});
+
+document.querySelector('.toggle input').checked = true;
+
+//================== сортировка ===========================//
+
+let myCont = document.querySelector('.card-row');
+let arrow = document.querySelector('.arrow-price');
+
+let toggleActive = document.querySelector('.sort');
+toggleActive.addEventListener('click', function (e) {
+   let sortPrice = document.querySelector('.sort-click-price');
+   e.target.classList.toggle('active');
+   if (sortPrice.classList.contains('active')) {
+      sortPrice = sortPriceUp();
+      arrow.classList.remove('rotate');
+   } else {
+      sortPrice = sortPriceDown();
+      arrow.classList.add('rotate');
+   }
+
+});
+
+
+function sortPriceUp() {
+   for (let i = 0; i < myCont.children.length; i++) {
+      for (let j = i + 1; j < myCont.children.length; j++) {
+         if (+myCont.children[i].getAttribute('data-price') > +myCont.children[j].getAttribute('data-price')) {
+            replacedNode = myCont.replaceChild(myCont.children[j], myCont.children[i]);
+            insertAfter(replacedNode, myCont.children[i]);
+
+         }
+      }
+   }
+}
+
+function sortPriceDown() {
+   for (let i = 0; i < myCont.children.length; i++) {
+      for (let j = i + 1; j < myCont.children.length; j++) {
+         if (+myCont.children[i].getAttribute('data-price') < +myCont.children[j].getAttribute('data-price')) {
+            replacedNode = myCont.replaceChild(myCont.children[j], myCont.children[i]);
+            insertAfter(replacedNode, myCont.children[i]);
+         }
+      }
+   }
+}
+
+function insertAfter(elem, refElem) {
+   return refElem.parentNode.insertBefore(elem, refElem.nextSibling);
+}
+
+// ================ выбор и отмена избранного =========== //
+
+let images = document.querySelectorAll('.img');
+
+function showImage1() {
+   for (let i = 0; i < images.length; i++) {
+      images[i].addEventListener('click', (e) => {
+         if (images[i] == e.target) {
+            images[i].src = '/img/star_bg.png';
+            images[i].classList.add('changeImage');
+         }
+      })
+   }
+}
+showImage1();
+
+function showImage2() {
+   for (let i = 0; i < images.length; i++) {
+      images[i].addEventListener('click', (e) => {
+         if (images[i] == e.target) {
+            images[i].src = '/img/star.png';
+         }
+      })
+   }
+}
+// showImage2();
